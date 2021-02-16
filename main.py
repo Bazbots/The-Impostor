44a7beefbddcbabdb4cb94178c9be997eade9b64
Baz <78729801+Bazbots@users.noreply.github.com>
2021-02-16 14:26:20 +0000

    1.2.6

--- main.py
+++ main.py
@@ -32,11 +32,10 @@ client = discord.Client()
 status = cycle([
     "Among Us on Discord! | Run $help or $commands for help!",
     "https://bazbots.github.io/Impostor-Bot/ | Run $website to gain a link!",
-    "Happy Valentines Day! | Run $help or $commands for help!",
-    "Version 1.2.3!",
+    "Happy Pancake Day! | Run $help or $commands for help!",
+    "Version 1.2.6!",
     "Vote for us here at https://top.gg/bot/759436027529265172",
-    "The GitHub Repository - $github for a link!"
-])
+    "The GitHub Repository | $github for a link!", "In MAXIMUM Servers | Join our Support Server For more Information"])
 
 #f"Among Us in {len(client.guilds)} servers!"
 
@@ -47,14 +46,14 @@ async def change_status():
 	print("Successfully changed status!")
 
 
-BOTVERSION = "1.2.3"
+BOTVERSION = "1.2.6"
 
 
 @client.event
 async def on_ready():
 	print("Loading...")
 	time.sleep(3)
-	print('Successfully booted {0.user}\nVersion 1.2.3'.format(client))
+	print('Successfully booted {0.user}\nVersion 1.2.6'.format(client))
 	time.sleep(2)
 	print("Booted at", current_time)
 	time.sleep(2)
@@ -63,7 +62,7 @@ async def on_ready():
 
 Gold_Tier = bool
 Diamond_Tier = bool
-Basic_Tier = True
+Basic_Tier = bool
 
 
 @client.event
@@ -106,46 +105,28 @@ async def feedback(self, ctx):
 async def on_message(message):
 	if message.author == client.user:
 		return
-	if message.content.startswith("$log"):
-		await message.channel.send("Logging...")
-		logger = logging.getLogger('discord')
-		logger.setLevel(logging.DEBUG)
-		handler = logging.FileHandler(filename='discord.log',
-		                              encoding='utf-8',
-		                              mode='w')
-		handler.setFormatter(
-		    logging.Formatter(
-		        '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-		logger.addHandler(handler)
-		await message.channel.send("Done!\nThanks for your help!")
 	if message.content.startswith('$help'):
 		await message.channel.send('Need help? Join my support server!\nhttps://discord.gg/Sun4mtFjwE\nOr you can run $commands to get help with commands!')
 	if message.content.startswith("$invite"):
 		await message.channel.send(":incoming_envelope:Invite me here!:incoming_envelope:\n https://discord.com/api/oauth2/authorize?client_id=759436027529265172&permissions=217579073&redirect_uri=https%3A%2F%2Fdiscord.gg%2FnzsyDWg&scope=bot")
 	if message.content.startswith("$about"):
-		await message.channel.send(":question:A bit about The Impostor:question:\n\n:lock:100% Safe and Secure:lock:\n:england:English:england:\n:white_check_mark:Sus:white_check_mark:\n:spy:Impossible for Data Breaching:spy:\n:gift:Celebrating Events All Year Round!:gift:")
+		await message.channel.send(":question:A bit about The Impostor:question:\n\n:lock:100% Safe and Secure:lock:\n:england:English:england:\n:white_check_mark:Sus:white_check_mark:\n:spy:Impossible for Data Breaching:spy:")
 	if message.content.startswith("$commands"):
-		await message.channel.send(":robot:Current Commands::robot:\nUse prefix `$`\nhelp\nabout\ninvite\nversion\nprefix\nwebsite\nvote\nservers\ncreator\ngithub\n\n\nSpecial Commands:\nUse the prefix `g$` for these commands\nevent")
+		await message.channel.send(":robot:Current Commands::robot:\nUse prefix `$`\nhelp\nabout\ninvite\nversion\nwebsite\nvote\nservers\ncreator\ngithub\ntier")
 	if message.content.startswith("$version"):
-		await message.channel.send(":rocket:Current Version::rocket:\n`1.2.3`\n\n\n:inbox_tray:What's new to this update::inbox_tray:\n:white_check_mark:Added the tier command again\n:white_check_mark:Added the upgrade command\n\n:clock3:What is still to come::clock3:\n:clock3:Solo Mode Among Us\n:clock3:Fixing the guild status issue\n\n:outbox_tray:What we removed::outbox_tray:\n:x:Nothing!")
-	if message.content.startswith("$prefix"):
-		await message.channel.send(":gear:We need a Database to change the prefix, it is something we will work on and will be released in a future update!\nUntil then, use the prefix $ or g$ for special commands!:gear:")
+		await message.channel.send(":rocket:Current Version::rocket:\n`1.2.6`\n\n\n:inbox_tray:What's new to this update::inbox_tray:\n:white_check_mark:Nothing\n\n:clock3:What is still to come::clock3:\n:clock3:Solo Mode Among Us\n:clock3:Fixing the guild status issue\n\n:outbox_tray:What we removed::outbox_tray:\n:x:The upgrade command\n:x:The Prefix command")
 	if message.content.startswith("$website"):
 		await message.channel.send(":desktop:Here is my Website::desktop:\nhttps://bazbots.github.io/Impostor-Bot/")
 	if message.content.startswith("$vote"):
-		await message.channel.send(":arrow_up:The bot is awaiting verification and can been found here::arrow_up:\nhttps://top.gg/bot/759436027529265172")
+		await message.channel.send(":arrow_up:The bot can been found here::arrow_up:\nhttps://top.gg/bot/759436027529265172")
 	if message.content.startswith("$servers"):
-		await message.channel.send(f"Currently playing Among Us in {len(client.guilds)} servers!")
+		await message.channel.send(f"Currently playing Among Us in **{len(client.guilds)}** servers!")
 	if message.content.startswith("$creator"):
 		await message.channel.send(":placard:The Impostor was a bot created by Baz!:placard:\n:one:This is actually his first ever coded bot!:one:\n:snake:It is a discord.py bot!:snake:\n:warning:The bot is not complete yet!:warning:\n:sparkling_heart:Show some love and join his server!:sparkling_heart:\n:calling: https://discord.gg/5jKA9kj :calling:")
-	if message.content.startswith("g$event"):
-		await message.channel.send(":calendar:Current Event is::calendar:\n:heart:Valentines Day!:heart:\n\n:sparkling_heart:Happy Valentines Day!:sparkling_heart:\n\nThe next event is...\n:woman:Mothers Day!:woman:")
 	if message.content.startswith("$github"):
 		await message.channel.send(":file_folder:Oke doke!:file_folder:\n:open_file_folder:Here is my GitHub Repo!:open_file_folder:\nhttps://github.com/Bazbots/Impostor-Bot")
 	if message.content.startswith("$tier"):
-		await message.channel.send(":free:You are in `Basic` Tier!:free:\nThis is Tier `1` out of 3!\n:free:Basic:free:\n:coin:Gold:coin:\n:gem:Diamond:gem:")
-	if message.content.startswith("$upgrade"):
-		await message.channel.send(":crown:The Tier system is coming soon!:crown:\n:question:What you can do at `Basic` Tier::question:\n:white_check_mark:Use all Basic commands (all under basic in $commands)\n:white_check_mark:You have access to certain modes (Standard and Crazy Colours)\n\n:coin:What you get when you reach `Gold` Tier::coin:\n:coin:Access to most modes(Jester, Troll and Terminator)\n:coin:A 10% higher chance of being Impostor\n\n:gem:What you get at `Diamond` Tier::gem:\n:gem:Access to all modes, even before they are released!\n:gem:25% higher chance of being Impostor\n:gem:Able to suggest modes for the bot!\n\n\n:crown:Soon, you will be able to unlock the other tiers!\n\nHey, you can unlock these tiers from giveaways on our support server!\nhttps://discord.gg/Sun4mtFjwE")
+		await message.channel.send(":free:You are in `Basic` Tier!:free:\nThis is Tier `1` out of 3!\n:free:Basic:free:\n:coin:Gold:coin:\n:gem:Diamond:gem:\n:question:What you can do at `Basic` Tier::question:\n:white_check_mark:Use all Basic commands (all under basic in $commands)\n:white_check_mark:You have access to certain modes (Standard and Crazy Colours)\n\n:coin:What you get when you reach `Gold` Tier::coin:\n:coin:Access to most modes(To be added soon)\n:coin:Multiplayer Mode\n:coin:A 10% higher chance of being Impostor\n\n:gem:What you get at `Diamond` Tier::gem:\n:gem:Access to all modes, even before they are released!\n:gem:25% higher chance of being Impostor\n:gem:Able to suggest modes for the bot!\n\n\n:crown:Soon, you will be able to unlock the other tiers!\n\nHey, you can unlock these tiers from giveaways on our support server!\nhttps://discord.gg/Sun4mtFjwE")
 
 
 
