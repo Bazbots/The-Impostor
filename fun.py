e27123deeb0d2fd7edcce7282715b43187c1d99b
Baz <78729801+Bazbots@users.noreply.github.com>
2021-03-05 13:20:05 +0000

    1.4.3

--- fun.py
+++ fun.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from colorama import Fore
 import random
 
-deaths = ["being brutally murdered", "being ejected into space", "being no scoped for a 180 headshot", "being oofed", "tripping over the impostor and falling into some conveniently placed lava", "having their neck snapped\nOuch!", "being repeatedly stabbed in the back", "being hacked\n`oh no`"]
+deaths = ["brutally murdered them", "ejected them into space", "no scoped them for a 180 headshot", "oofing them", "tripping them over and them making them fall into some conveniently placed lava", "snapped their neck!\nOuch!", "repeatedly stabbed them in the back", "hacked all of their `vbucks`"]
 
 class Fun(commands.Cog):
 
@@ -33,7 +33,7 @@ class Fun(commands.Cog):
   
   @commands.command()
   async def kill(self, ctx, username):
-    await ctx.send(f"{username} was killed by {ctx.author.mention}" + "\n" + f"They died by {random.choice(deaths)}")
+    await ctx.send(f"{username} was killed by {ctx.author.mention} who {random.choice(deaths)}")
 
   @kill.error
   async def no_username(self, ctx, error):
