e34ec04965d545f3ac129bf970513f6648eff652
Baz <78729801+Bazbots@users.noreply.github.com>
2021-03-08 18:20:34 +0000

    1.4.4

--- basic.py
+++ basic.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 from colorama import Fore
+from datetime import datetime
+
+now = datetime.now()
+
+boot_time = now.strftime("%H:%M:%S")
 
 class Basic(commands.Cog):
 
@@ -15,7 +20,7 @@ class Basic(commands.Cog):
 
   @commands.command()
   async def help(self, ctx):
-	  await ctx.send("Need help? Join my support server!\nhttps://discord.gg/Sun4mtFjwE\nOr you can view all the current commands below!\n\n\n:robot:Current Commands::robot:\nUse prefix `$`\nhelp\nabout\ninvite\nversion\nfeedback\nwebsite\nvote\nservers\ncreator\ngithub\ntier {current/basic/gold/diamond}\nping\neject {user} {role}\nreport {your username} {problem}\nsus")
+	  await ctx.send("Need help? Join my support server!\nhttps://discord.gg/Sun4mtFjwE\nOr you can view all the current commands below!\n\n\n:robot:Current Commands::robot:\nUse prefix `$`\nhelp\nabout\ninvite\nversion\nfeedback\nwebsite\nvote\nservers\ncreator\ngithub\ntier {current/basic/gold/diamond}\nping\neject {user} {role}\nsus\n$kill {username}")
 
   @commands.command()
   async def invite(self, ctx):
@@ -27,7 +32,7 @@ class Basic(commands.Cog):
   
   @commands.command()
   async def version(self, ctx):
-	  await ctx.send(":rocket:Current Version::rocket:\n`1.4.3`\n\n\n:inbox_tray:What's new to this update::inbox_tray:\n:white_check_mark:$kill (Thanks <@513283671353196585> for the suggestion)\n\n:clock3:What is still to come::clock3:\n:clock3:Solo Mode Among Us\n:clock3:Fixing the guild status issue\n:clock3:The return of $changeprefix\n$play and $modes\n\n:outbox_tray:What we removed::outbox_tray:\n:x:Nothing")
+	  await ctx.send(":rocket:Current Version::rocket:\n`1.4.4`\n\n\n:inbox_tray:What's new to this update::inbox_tray:\n:white_check_mark:Creator Exclusive commands\n\n:clock3:What is still to come::clock3:\n:clock3:Solo Mode Among Us\n:clock3:Fixing the guild status issue\n$play and $modes\n\n:outbox_tray:What we removed::outbox_tray:\n:x:Nothing")
   
   @commands.command()
   async def website(self, ctx):
@@ -45,16 +50,6 @@ class Basic(commands.Cog):
   async def feedback(self, ctx):
 	  await ctx.send(":pencil:Please answer this short survey to let us know how you feel about the bot::pencil:\nhttps://docs.google.com/forms/d/e/1FAIpQLSeS_fcVh5_GRBmYCFw5qkxU29lSLU1zsTkioePy7Kp8roTVig/viewform?usp=sf_link")
 
-  @commands.command()
-  async def report(self, ctx, username, problem):
-    print(Fore.GREEN + f"{username} has a problem: {problem}")
-    await ctx.send("Done!\nThanks for reporting this issue, we will look into it!")
-
-  @report.error
-  async def no_prob_or_user(self, ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-      await ctx.send(f":x:Error:x:\n:information_source:{error}")
-      print(Fore.GREEN + f"Error: {error}")
 
   
 
@@ -62,3 +57,4 @@ class Basic(commands.Cog):
 
 def setup(client):
   client.add_cog(Basic(client))
+  print(Fore.GREEN + "Basic Cog added!")
