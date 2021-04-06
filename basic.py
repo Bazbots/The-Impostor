5d1e07044ff9e0b9cbe9a8c8bfa8ad9da477c5a7
Baz <78729801+Bazbots@users.noreply.github.com>
2021-04-06 11:28:40 +0100

    1.5.5

--- basic.py
+++ basic.py
@@ -4,11 +4,6 @@ from colorama import Fore
 from datetime import datetime
 import random
 
-now = datetime.now()
-
-boot_time = now.strftime("%H:%M:%S")
-
-sponsors = ["\n\nHey! You can join Fritzwinger's discord server here!\nhttps://discord.gg/wYC6dtUM5J", "\n\nHey! You can join Baz's discord server here!\nhttps://discord.gg/5jKA9kj", "\n\nHey! You should check out Milxq ok's channel! https://www.youtube.com/channel/UCA65XYkOjhXc1G3pko2bpbw", "", "", "", "", "", "", "", ""]
 
 
 class Basic(commands.Cog):
@@ -25,51 +20,56 @@ class Basic(commands.Cog):
 
   @commands.command()
   async def help(self, ctx):
-	  await ctx.send("Need help? Join my support server!\nhttps://discord.gg/Sun4mtFjwE\nOr you can view all the current commands below!\n\n\n:robot:Current Commands::robot:\nUse prefix `$`\n\nInformative Commands:\nhelp\nabout\ninvite\nversion\nfeedback\nwebsite\nvote\ncreator\ngithub\nsponsors\nping\n\nFun Commands:\neject {user} {role}\nsus\nkill {username}\n\nGame Commands (Disabled):\nhost\nmute\nunmute\ndead\ngame_mute\nstart\n\nTasks:\nscan\ndownload\nweatherdownload\ninspect")
+    embedTwo = discord.Embed(
+        title="Help",
+        description="Join my support server here:\nhttps://discord.gg/Sun4mtFjwE\n\nUse the prefix `$` or `@The Impostor`",
+        colour=discord.Colour.red()
+      )
+    embedTwo.add_field(name="Informative Commands",value="help\ninfo\nabout\nversion\nfeedback\ncreator\nping", inline=True)
+    embedTwo.add_field(name="Fun commands", value="eject {user} {role}\nsus\nkill {username}\nvent\nsabotage", inline=True)
+    embedTwo.add_field(name="Tasks", value="scan\ndownload\nweather\ninspect")
+    embedTwo.set_thumbnail(url="https://cdn1.iconfinder.com/data/icons/logos-brands-in-colors/231/among-us-player-red-512.png")
+    embedTwo.set_footer(text="© Baz - The Impostor - Among Us bot for Discord")
+    await ctx.send(embed=embedTwo)
 
   @commands.command()
-  async def invite(self, ctx):
-	  await ctx.send(":incoming_envelope:Invite me here!:incoming_envelope:\n https://discord.com/api/oauth2/authorize?client_id=759436027529265172&permissions=8&redirect_uri=https%3A%2F%2Fbazbots.github.io%2FImpostor-Bot&response_type=code&scope=identify%20email%20bot")
+  async def info(self, ctx):
+    print(f"{ctx.author} has pinged the info command")
+    embedTen = discord.Embed(
+      title="Bot Information",
+      colour=discord.Colour.red()
+      )
+    embedTen.add_field(name="Invite Link", value="https://discord.com/api/oauth2/authorize?client_id=759436027529265172&permissions=8&redirect_uri=https%3A%2F%2Fbazbots.github.io%2FThe-Impostor&response_type=code&scope=identify%20email%20bot%20applications.commands", inline=True)
+    embedTen.add_field(name="Website", value="https://bazbots.github.io/The-Impostor", inline=True)
+    embedTen.set_thumbnail(url="https://cdn1.iconfinder.com/data/icons/logos-brands-in-colors/231/among-us-player-red-512.png")
+    embedTen.set_footer(text="© Baz - The Impostor - Among Us bot for Discord")
+    embedTen.add_field(name="Support Server Link:", value="https://discord.gg/Sun4mtFjwE", inline=True)
+    embedTen.add_field(name="Voting link:", value="https://top.gg/bot/759436027529265172/vote", inline=True)
+    embedTen.add_field(name="Github Repository", value="https://github.com/Bazbots/The-Impostor", inline=True)
+    await ctx.send(embed=embedTen)
       
   @commands.command()
   async def about(self, ctx):
-	  await ctx.send(f":question:A bit about The Impostor:question:\n\n:lock:100% Safe and Secure:lock:\n:england:English:england:\n:white_check_mark:Sus:white_check_mark:\n:spy:Impossible for Data Breaching:spy:{random.choice(sponsors)}")
+	  await ctx.send(":question:A bit about The Impostor:question:\n\n:lock:100% Safe and Secure:lock:\n:england:English:england:\n:white_check_mark:Sus:white_check_mark:\n:spy:Impossible for Data Breaching:spy:")
   
   @commands.command()
   async def version(self, ctx):
-	  await ctx.send(f":rocket:Current Version::rocket:\n`1.5.0`\n\n\n:inbox_tray:What's new to this update::inbox_tray:\n:white_check_mark:$download, $weatherdownload, $inspect\n\n:clock3:What is still to come::clock3:\n:clock3:Fixing the servers I'm in status\n\n:outbox_tray:What we removed::outbox_tray:\n:x:Disabled host commands until further notice{random.choice(sponsors)}")
-
-  @commands.command()
-  @commands.is_owner()
-  async def cv(self, ctx):
-    await ctx.send(":rocket:Current Version::rocket:\n`1.5.0`\n\n\n:inbox_tray:What's new to this update::inbox_tray:\n:white_check_mark:$download, $weatherdownload, $inspect\n\n:clock3:What is still to come::clock3:\n:clock3:Fixing the servers I'm in status\n\n:outbox_tray:What we removed::outbox_tray:\n:x:Disabled host commands until further notice")
-
+	  await ctx.send(":rocket:Current Version::rocket:\n`1.5.5`\n\n\n:inbox_tray:What's new to this update::inbox_tray:\n:white_check_mark:$sabotage\n\n:clock3:What is still to come::clock3:\n:clock3:Fixing the servers I'm in status\n\n:outbox_tray:What we removed::outbox_tray:\n:x:Nothing!")
   
-  @commands.command()
-  async def website(self, ctx):
-	  await ctx.send(f":desktop:Here is my Website::desktop:\nhttps://bazbots.github.io/Impostor-Bot/{random.choice(sponsors)}")
-
-  @commands.command()
-  async def github(self, ctx):
-	  await ctx.send(f":file_folder:Oke doke!:file_folder:\n:open_file_folder:Here is my GitHub Repo!:open_file_folder:\nhttps://github.com/Bazbots/Impostor-Bot{random.choice(sponsors)}")
 
   @commands.command()
   async def creator(self, ctx):
 	  await ctx.send(":placard:The Impostor was a bot created by Baz!:placard:\n:one:This is actually his first ever coded bot!:one:\n:snake:It is a discord.py bot!:snake:\n:warning:The bot is not complete yet!:warning:\n:sparkling_heart:Show some love and join his server!:sparkling_heart:\n:calling: https://discord.gg/5jKA9kj :calling:")
+  
+  
+ 
+ 
 
   @commands.command()
   async def feedback(self, ctx):
-	  await ctx.send(":pencil:Please answer this short survey to let us know how you feel about the bot::pencil:\nhttps://docs.google.com/forms/d/e/1FAIpQLSeS_fcVh5_GRBmYCFw5qkxU29lSLU1zsTkioePy7Kp8roTVig/viewform?usp=sf_link LLP")
+	  await ctx.send(":pencil:Please answer this short survey to let us know how you feel about the bot::pencil:\nhttps://docs.google.com/forms/d/e/1FAIpQLSeS_fcVh5_GRBmYCFw5qkxU29lSLU1zsTkioePy7Kp8roTVig/viewform?usp=sf_link")
 	  print(f"{ctx.author} has triggered the feedback command")
-  
-  @commands.command()
-  async def sponsors(self, ctx):
-    await ctx.send("Here are our current sponsors!\nTheir product will pop out throughout some of your commands!\n\nFritzwinger - Discord Server - https://discord.gg/wYC6dtUM5J\nCamila - YouTube Channel - https://www.youtube.com/channel/UCGQgRVgKexKmOfIA4lrFG9Q\nMilxq ok - YouTube channel - https://www.youtube.com/channel/UCA65XYkOjhXc1G3pko2bpbw\n\nHey, want to be a sponsor?\nFill out this form as to why you should be one!\nThere are now 0 slots left! The slots will reset after this month!\nhttps://forms.gle/vpY9PToyLoFYTQeaA")
-    print(f"{ctx.author} pinged $sponsors")
-  
-  @commands.command()
-  async def vote(self, ctx):
-    await ctx.send(":arrow_up:You can vote for the bot here::arrow_up:\nhttps://top.gg/bot/759436027529265172/vote")
+	    
 
 
 
