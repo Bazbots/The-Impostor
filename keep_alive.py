57d87b9d061aa4c279c534f5f2ebf16b56120e1a
Baz <78729801+Bazbots@users.noreply.github.com>
2021-04-06 11:27:55 +0100

    1.5.5

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from threading import Thread
-app = Flask("")
+
+app = Flask("The Impostor")
 
 @app.route("/")
 def home():
-  return "Hello I am alive!"
+  return "When the Impostor is sus"
 def run():
   app.run(host="0.0.0.0",port=8080)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+  server = Thread(target=run)
+  server.start()
