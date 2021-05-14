61ec59d607fc54f092183f13d1dbaf3890eff911
Baz <78729801+Bazbots@users.noreply.github.com>
2021-05-14 15:25:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ from colorama import Fore
 from replit import db
 import random
 import json
+import patreon
+
+
 
 now = datetime.now()
 
 boot_time = now.strftime("%H:%M:%S")
 
 client = commands.Bot(
-    command_prefix=["$", "<@759436027529265172>", "<@759436027529265172> "], description="An Among Us bot for Discord!", case_insensitive=True)
+    command_prefix=["$", "<@759436027529265172>", "<@759436027529265172> ", "<@!759436027529265172>", "<@!759436027529265172> ", "<@&759436027529265172> ", "<@&759436027529265172>"], description="An Among Us bot for Discord!", case_insensitive=True)
 client.remove_command("help")
 
+
+__version__ = "X.X.X"
 
 
 @client.event
@@ -32,9 +37,9 @@ async def on_guild_join(guild):
   for channel in guild.text_channels:
     if channel.permissions_for(guild.me).send_messages:
       embedHi = discord.Embed(
-			    title=":mailbox:Hi there!:mailbox:",
+			    title="Thanks for adding me!",
 			    description=
-			    f"<:impostor:774673531786625024>I am the Impostor - a bot created by Baz!<:impostor:774673531786625024>\n\n<:noice:751384305464377375>You can join my support server by running $help and you can view all of my commands here as well!<:noice:751384305464377375>\n\n:partying_face:Have fun!:partying_face:\n\n\n<:ping:757276110252670986>When you added this bot, it was in version {__version__}<:ping:757276110252670986>",
+			    f"<:impostor:774673531786625024>I am the Impostor - a bot created by Baz!<:impostor:774673531786625024>\n\n<:noice:751384305464377375>You can join my support server by running $help and you can view all of my commands here as well!<:noice:751384305464377375>\n\n<:patreon:839897502925062165> Feel free to go to https://www.patreon.com/theimpostor do gain access to cool premium commands! <:patreon:839897502925062165>\n:partying_face:Have fun!:partying_face:\n\n\n<:ping:757276110252670986>When you added this bot, it was in version {__version__}<:ping:757276110252670986>",
 			    colour=discord.Colour.red())
       embedHi.set_thumbnail(
 			    url=
@@ -55,19 +60,23 @@ async def on_guild_remove(guild):
 
 
 
+
 @client.event
 async def on_ready():
   print(Fore.BLUE + f'Successful login to {client.user}\nVersion {__version__}\nBot is in {len(client.guilds)} servers\nClient ID: {client.user.id}\nRemember to add 1 hour to time: {boot_time}')
   change_status.start()
-  await asyncio.sleep(1)
 
+  
+
+  
 
 @tasks.loop(seconds=600)
 async def change_status():
   await client.change_presence(activity=discord.Game(next(status)))
   print(Fore.GREEN + "Successfully changed status!")
 
-status = cycle(["Among Us on Discord! | $help, $info",
+
+status = cycle(["Among Us on Discord! | $help, $info", "https://www.patreon.com/theimpostor | $patreon",
     "https://bazbots.github.io/The-Impostor | $info",
     f"Version {__version__} | $version",
     "Vote for the bot by running $vote {site} | $info",
@@ -76,10 +85,10 @@ status = cycle(["Among Us on Discord! | $help, $info",
 ])
 
 
-
 @client.command()
 async def load(ctx, extension):
 	client.load_extension(f"cogs.{extension}")
+  
 
 
 @client.command()
