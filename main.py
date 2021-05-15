ea90830a45ea50081a39fd6bc6d826c9fcb8de97
Baz <78729801+Bazbots@users.noreply.github.com>
2021-05-15 15:05:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ now = datetime.now()
 
 boot_time = now.strftime("%H:%M:%S")
 
+
 client = commands.Bot(
     command_prefix=["$", "<@759436027529265172>", "<@759436027529265172> ", "<@!759436027529265172>", "<@!759436027529265172> ", "<@&759436027529265172> ", "<@&759436027529265172>"], description="An Among Us bot for Discord!", case_insensitive=True)
 client.remove_command("help")
@@ -31,9 +32,16 @@ client.remove_command("help")
 __version__ = "X.X.X"
 
 
+
 @client.event
 async def on_guild_join(guild):
   print(Fore.GREEN + f"I have joined {guild}" + Fore.RESET)
+  channel = client.get_channel(838465155582394390)
+  console = discord.Embed(
+    title="Console",
+    description=f"I have joined {guild}"
+  )
+  await channel.send(embed=console)
   for channel in guild.text_channels:
     if channel.permissions_for(guild.me).send_messages:
       embedHi = discord.Embed(
@@ -56,7 +64,12 @@ async def on_guild_join(guild):
 @client.event
 async def on_guild_remove(guild):
   print(Fore.GREEN + f"I have left {guild}" + Fore.RESET)
-
+  channel = client.get_channel(838465155582394390)
+  console = discord.Embed(
+    title="Console",
+    description=f"I have left {guild}"
+  )
+  await channel.send(embed=console)
 
 
 
@@ -66,24 +79,22 @@ async def on_ready():
   print(Fore.BLUE + f'Successful login to {client.user}\nVersion {__version__}\nBot is in {len(client.guilds)} servers\nClient ID: {client.user.id}\nRemember to add 1 hour to time: {boot_time}')
   change_status.start()
 
-  
 
   
+
+
 
 @tasks.loop(seconds=600)
 async def change_status():
-  await client.change_presence(activity=discord.Game(next(status)))
+  await client.change_presence(activity=random.choice(activities))
   print(Fore.GREEN + "Successfully changed status!")
 
 
-status = cycle(["Among Us on Discord! | $help, $info", "https://www.patreon.com/theimpostor | $patreon",
-    "https://bazbots.github.io/The-Impostor | $info",
-    f"Version {__version__} | $version",
-    "Vote for the bot by running $vote {site} | $info",
-    "The GitHub Repository | $info", "What do you think? | $feedback",
-    "$help, $info", "Use the prefix $ or @The Impostor", "Created by Baz!"
-])
 
+status = ["Among Us on Discord! | $help, $info", "https://www.patreon.com/theimpostor | $patreon","https://bazbots.github.io/The-Impostor | $info", f"Version {__version__} | $version","Vote for the bot by running $vote {site} | $info","The GitHub Repository | $info", "What do you think? | $feedback","$help, $info","Use the prefix $ or @The Impostor", "Created by Baz!"]
+
+
+activities = [discord.Game(name=random.choice(status)), discord.Activity(type=discord.ActivityType.competing, name=random.choice(status)), discord.Activity(type=discord.ActivityType.watching, name=random.choice(status)), discord.Activity(type=discord.ActivityType.listening, name=random.choice(status)), discord.Streaming(name=random.choice(status), url="https://twitch.tv/itzbazlol")]
 
 @client.command()
 async def load(ctx, extension):
